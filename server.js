9acfb6aacb7da758773973789edc1ed1923bbecd
Kevin Welcher <kaw2k@virginia.edu>
2014-11-24 18:13:39 -0500

    Again

--- server.js
+++ server.js
@@ -12,18 +12,19 @@ app.set('port', Number(process.env.PORT || 2000));
 //app.set('views', __dirname + '/../views');
 //app.set('view engine', 'jade');
 //app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
 //app.use(express.static(path.join(__dirname, './public')));
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
+
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
 
 
 //# Routes -----------------------------------------------------------------------
